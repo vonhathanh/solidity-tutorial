d24143048f5605429ad4ba380e5248150d697f33
hanhvn <vonhathanh@gmail.com>
2022-04-21 13:04:42 +0700

    add script to view log

---
+++ scripts/demo_logs.js
@@ -0,0 +1,23 @@
+const { ethers, Contract } = require("ethers");
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://data-seed-prebsc-1-s1.binance.org:8545/"
+);
+provider.getBlockNumber().then((result, err) => {
+  console.log(result);
+});
+
+let topic = ethers.utils.id("Voted(address,address,uint256)");
+
+let filter = {
+  address: "0x37157AB17E19B9fc5644F576D4265c35E04aA66c",
+  topics: [topic],
+};
+
+provider.on(filter, (result) => {
+  console.log(result);
+});
+
+// abi = ["event Transfer(address indexed src, address indexed dst, uint val)"];
+
+// contract = new Contract("0x26C84EAeC7735a3B263bde1368f586791DBB978A", abi, provider);
+// console.log(contract.filters.Transfer("0xEbEC1c6317dC6fD6130DA4E9ce4FaFb84e698401"));
